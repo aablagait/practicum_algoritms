1a636e42244256a89bc8379a746d689e48d7217d
Aleksey Bocharov <aleksej.bocharov9@gmail.com>
2023-12-27 19:05:25 +0300

    modern 'if' and clean function count_iter_delivey

--- delivery_service.py
+++ delivery_service.py
@@ -1,33 +1,35 @@
-# Время посылки: 26 дек 2023, 17:24:28
-# ID: 103907180
+# Время посылки: 27 дек 2023, 18:55:34
+# ID: 103973719
 # Задача: A
 # Компилятор: Python 3.12.1
 # Вердикт: OK
-# Время: 52ms
-# Память: 4.27Mb
+# Время: 55ms
+# Память: 4.55Mb
 import sys
 
 
-def count_iter_delivery() -> int:
-    weight: list = sys.stdin.readline().rstrip().split()
-    weight: list = list(map(int, weight))
+def count_iter_delivery(weight, limit) -> int:
     weight.sort()
-    limit: int = int(sys.stdin.readline().rstrip())
     count_iter: int = 0
     left: int = 0
     right: int = len(weight) - 1
 
     while left <= right:
-        if weight[left] + weight[right] > limit:
-            count_iter += 1
-            right -= 1
-        else:
-            count_iter += 1
-            right -= 1
+        # if the weight of two robots is less than the limit
+        # move the left pointer
+        if weight[left] + weight[right] <= limit:
             left += 1
+        # always move the right pointer
+        right -= 1
+        # and always add 1 to counter
+        count_iter += 1
 
     return count_iter
 
 
 if __name__ == "__main__":
-    print(count_iter_delivery())
+    params: list = sys.stdin.readline().rstrip().split()
+    params: list = list(map(int, params))
+    max_mass: int = int(sys.stdin.readline().rstrip())
+    result = count_iter_delivery(params, max_mass)
+    print(result)
