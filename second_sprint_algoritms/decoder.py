ec225fa8dbb758f8f5a77837ecb2e6b610a60e79
Aleksey Bocharov <aleksej.bocharov9@gmail.com>
2024-01-09 21:45:40 +0300

    work version with ok on contest

--- second_sprint_algoritms/decoder.py
+++ second_sprint_algoritms/decoder.py
@@ -5,16 +5,19 @@ def decoder(comand):
     numbers = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '0')
     i = 0
     result = []
-    coef = [1]
+    coefficient: list = [1]
+    coef = ''
     while i < len(comand):
         if comand[i] == '[':
+            coefficient.append(int(coef))
+            coef = ''
             str_in_bracket, swipe_i = decoder(comand[i+1:])
-            result.append(coef.pop() * str_in_bracket)
+            result.append(coefficient.pop() * str_in_bracket)
             i += swipe_i
         elif comand[i] == ']':
-            return coef.pop() * ''.join(result), i+2
+            return coefficient.pop() * ''.join(result), i+2
         elif comand[i] in numbers:
-            coef.append(int(comand[i]))
+            coef += comand[i]
             i += 1
         else:
             result.append(comand[i])
@@ -24,11 +27,4 @@ def decoder(comand):
 
 if __name__ == "__main__":
     comand: list = list(sys.stdin.readline().rstrip())
-    # comand = '3[a]2[bc]'
-    expected = 'aaabcbc'
-    print(decoder(comand))
-    print(decoder(comand)[0] == expected)
     print(decoder(comand)[0])
-
-
-
